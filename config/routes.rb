b12ad6b1499ae1d633a4c2827fb7c92d70be3dc9
Joyce Martinez <joycetezanos@gmail.com>
2020-08-13 18:12:23 -0500

    Mister Cocktail app done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :ingredients
-  resources :cocktails
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
+  resources :doses, only: [:destroy]
 end
